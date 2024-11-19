3b1751ec5a7be1b5e6618bea81b68fc391a39c25
amir1986651 <a.zareeeee1@gmail.com>
2024-11-19 22:33:18 +0330

    Update hadi4668334.sh

--- hadi4668334.sh
+++ hadi4668334.sh
@@ -1,1 +1,6 @@
+hysteria2://ACoMimSKNq@45.156.23.69:24206?obfs=salamander&obfs-password=jdksiew52ekdwowk863djhd&insecure=1#hys2%2Bech%2Bpqs
+
+hysteria2://ACoMimSKNq@45.156.23.69:14965?obfs=salamander&obfs-password=hhdu662hnnxbi75gdes5w&insecure=1#hys2%2Bech
+
+hysteria2://ACoMimSKNq@45.156.23.69:24065?obfs=salamander&obfs-password=njtfgr4esf8omnbgft46d&insecure=1#hys2
 
