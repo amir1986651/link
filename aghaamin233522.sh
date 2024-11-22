35190d86d43c7838542225b06ac2c0753f88535e
amir1986651 <a.zareeeee1@gmail.com>
2024-11-22 23:11:32 +0330

    Update aghaamin233522.sh

--- aghaamin233522.sh
+++ aghaamin233522.sh
@@ -1,6 +1,5 @@
 
-
-
-66
-
+hysteria2://jE54nIUoho@45.156.23.69:24206?obfs=salamander&obfs-password=jdksiew52ekdwowk863djhd&insecure=1#hys2%2Bech%2Bpqs
+hysteria2://jE54nIUoho@45.156.23.69:14965?obfs=salamander&obfs-password=hhdu662hnnxbi75gdes5w&insecure=1#hys2%2Bech
+hysteria2://jE54nIUoho@45.156.23.69:24065?obfs=salamander&obfs-password=njtfgr4esf8omnbgft46d&insecure=1#hys2
 
